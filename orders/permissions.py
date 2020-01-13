6b6f3d9ce42fe75fd4142f418a419da11e20deaf
Islam Mesha <eng.islammesha@gmail.com>
2020-01-13 21:47:09 +0200

    Add some comments for permissions.

--- orders/permissions.py
+++ orders/permissions.py
@@ -3,6 +3,8 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsDeliveredOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
+        # Grant the customer to read orders.
         if request.method in SAFE_METHODS:
             return True
+        # If the order status is delivered customer must not be granted.
         return bool(False if obj.status == "delivered" else True)
